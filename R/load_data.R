8edf9fdcdb806177d026a855478e042307665322
Joscha Legewie <joscha.legewie@gmail.com>
2018-11-26 19:36:32 -0500

    Fix `load_data_decennial`

--- R/load_data.R
+++ R/load_data.R
@@ -274,19 +274,10 @@ load_data_decennial <- function(geography, variables, key, year,
     vars_to_get <- paste0(var, ",NAME")
   }
 
-  if (year == 2010) {
-    base <- paste0("https://api.census.gov/data/",
-                   year,
-                   "/dec/",
-                   sumfile)
-  } else {
-    base <- paste0("https://api.census.gov/data/",
+  base <- paste0("https://api.census.gov/data/",
                    year,
                    "/",
                    sumfile)
-  }
-
-
 
 
   for_area <- paste0(geography, ":*")
