9da7c8ef5efceccaa4891321898949c01080ab63
Matt Herman <mfherman@gmail.com>
2021-03-17 14:29:03 -0400

    Add option to get geometry via get_flows (#356)
    * add option to get point geometries of centroids of origin and destination for get_flows()

--- R/flows.R
+++ R/flows.R
@@ -26,12 +26,12 @@
 #'   character CBSA GEOIDs are accepted. When specifying CBSAs, geography must
 #'   be set to `"cbsa"` and `state` and `county` must be `NULL`. Defaults to
 #'   NULL.
-#' @param geometry if FALSE (the default), return a regular tibble of ACS data.
-#'   if TRUE, uses the tigris package to return an sf tibble with simple feature
-#'   geometry in the `geometry` column. **NOT YET IMPLEMENTED**
-#' @param shift_geo if TRUE, returns geometry with Alaska and Hawaii shifted for
-#'   thematic mapping of the entire US. Geometry was originally obtained from
-#'   the albersusa R package. **NOT YET IMPLEMENTED**
+#' @param geometry if FALSE (the default), return a tibble of ACS Migration
+#'   Flows data. If TRUE, return an sf object with the centroids of both origin
+#'   and destination as `sfc_POINT` columns. The origin point feature is
+#'   returned in a column named `centroid1` and is the active geometry column in
+#'   the sf object. The destination point feature is returned in the `centroid2`
+#'   column.
 #' @param key Your Census API key. Obtain one at
 #'   \url{http://api.census.gov/data/key_signup.html}
 #' @param moe_level The confidence level of the returned margin of error.  One
@@ -53,9 +53,9 @@
 #' @export
 get_flows <- function(geography, variables = NULL, year = 2018, output = "tidy",
                       state = NULL, county = NULL, cbsa = NULL, geometry = FALSE,
-                      shift_geo = FALSE, key = NULL, moe_level = 90,
-                      show_call = FALSE, ...) {
+                      key = NULL, moe_level = 90, show_call = FALSE, ...) {
 
+  # a bunch of checks to make sure the get_flows() call is specified correctly
   if (Sys.getenv('CENSUS_API_KEY') != '') {
     key <- Sys.getenv('CENSUS_API_KEY')
   } else if (is.null(key)) {
@@ -88,13 +88,14 @@ get_flows <- function(geography, variables = NULL, year = 2018, output = "tidy",
   }
 
   if (geography == "metropolitan statistical area/micropolitan statistical area" && !(is.null(state) && is.null(county))) {
-    stop("When requesting CBSA data, state and county must be NULL.", call. = FALSE)
+    stop("When requesting CBSA data, `state` and `county` must be NULL.", call. = FALSE)
   }
 
   if (year < 2010) {
     stop("Migration flows are available via API beginning in 2010", call. = FALSE)
   }
 
+  # if different moe level is specified, calculate factor to adjust by
   if (moe_level == 90) {
     moe_factor <- 1
   } else if (moe_level == 95) {
@@ -105,29 +106,16 @@ get_flows <- function(geography, variables = NULL, year = 2018, output = "tidy",
     stop("`moe_level` must be one of 90, 95, or 99.", call. = FALSE)
   }
 
-  cache <- getOption("tigris_use_cache", FALSE)
-
-  # if (geometry) {
-  #   if (shift_geo) {
-  #     message("Using feature geometry obtained from the albersusa package")
-  #   } else if (!shift_geo && !cache) {
-  #     message("Downloading feature geometry from the Census website.  To cache shapefiles for use in future sessions, set `options(tigris_use_cache = TRUE)`.")
-  #   }
-  # }
-  #
-  # if (shift_geo && !geometry) {
-  #   stop("`shift_geo` is only available when requesting feature geometry with `geometry = TRUE`",
-  #        call. = FALSE)
-  # }
-
   # for every call we want these variables
- always_vars <- c("GEOID1", "GEOID2", "FULL1_NAME", "FULL2_NAME",
-                  "MOVEDIN", "MOVEDIN_M", "MOVEDOUT", "MOVEDOUT_M")
+  always_vars <- c("GEOID1", "GEOID2", "FULL1_NAME", "FULL2_NAME",
+                   "MOVEDIN", "MOVEDIN_M", "MOVEDOUT", "MOVEDOUT_M",
+                   "MOVEDNET", "MOVEDNET_M")
 
   # if additional variables are requested, combine with always vars
   # and remove vars duplicated in variables specified
   variables <- c(always_vars, variables[!variables %in% always_vars])
 
+  # pull data from api
   dat <- load_data_flows(
     geography = geography,
     variables = variables,
@@ -139,75 +127,26 @@ get_flows <- function(geography, variables = NULL, year = 2018, output = "tidy",
     show_call = show_call
     )
 
+  # do some reshaping if requested and adjust moe if needed
   if (output == "tidy") {
     dat <- dat %>%
       tidyr::pivot_longer(cols = where(is.numeric), names_to = "variable") %>%
       tidyr::separate(.data$variable, into = c("variable", "type"), sep = "_", fill = "right") %>%
-      mutate(type = ifelse(is.na(.data$type), "estimate", "moe")) %>%
+      dplyr::mutate(type = ifelse(is.na(.data$type), "estimate", "moe")) %>%
       tidyr::pivot_wider(names_from = .data$type, values_from = .data$value) %>%
-      mutate(moe = .data$moe * moe_factor)
+      dplyr::mutate(moe = .data$moe * moe_factor)
   } else if (output == "wide") {
     dat <- dat %>%
       mutate(dplyr::across(dplyr::ends_with("_M"), ~ .x * moe_factor))
   }
 
-return(dat)
-#
-#   if (geometry) {
-#
-#     if (shift_geo) {
-#
-#       if (!is.null(state)) {
-#         stop("`shift_geo` is only available when requesting geometry for the entire US", call. = FALSE)
-#       }
-#
-#       message("Please note: Alaska and Hawaii are being shifted and are not to scale.")
-#
-#       if (geography == "state") {
-#
-#         geom <- tidycensus::state_laea
-#
-#       } else if (geography == "county") {
-#
-#         geom <- tidycensus::county_laea
-#
-#         if (year > 2014) {
-#           # Account for change from Shannon County, SD to Oglala Lakota County
-#           # and the new Kusilvak Census Area in AK
-#           geom$GEOID[geom$GEOID == "46113"] <- "46102"
-#           geom$GEOID[geom$GEOID == "02270"] <- "02158"
-#         }
-#
-#       } else {
-#         stop("`shift_geo` is only available for states and counties", call. = FALSE)
-#       }
-#
-#     } else {
-#
-#       geom <- suppressMessages(use_tigris(geography = geography, year = year,
-#                                           state = state, county = county, ...))
-#     }
-#
-#     if (! keep_geo_vars) {
-#
-#       geom <- select(geom, GEOID, geometry)
-#
-#     }
-#
-#     if (shift_geo) {
-#       out <- inner_join(geom, dat2, by = "GEOID") %>%
-#         st_as_sf()
-#     } else {
-#       out <- right_join(geom, dat2, by = "GEOID") %>%
-#         st_as_sf()
-#     }
-#
-#     return(out)
-#
-#   } else {
-#
-#     return(dat2)
-#
-#   }
-
+  # join to centroid data file twice to get point geometry of origin and destination centroids
+  # convert to sf object making origin centroid active geometry col
+  if (geometry) {
+    dat <- dat %>%
+      dplyr::left_join(tidycensus:::centroids, by = c("GEOID1" = "GEOID")) %>%
+      dplyr::left_join(tidycensus:::centroids, by = c("GEOID2" = "GEOID"), suffix = c("1", "2")) %>%
+      sf::st_as_sf(sf_column_name = "centroid1")
+  }
+  return(dat)
 }
