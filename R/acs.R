28b1ebcc7bdb63c34f96ffe448f396166368f7c2
rudeboybert <albert.ys.kim@gmail.com>
2021-11-15 07:54:43 -0500

    Add parentheses to mentions of census_api_key()

--- R/acs.R
+++ R/acs.R
@@ -155,7 +155,7 @@ get_acs <- function(geography, variables = NULL, table = NULL, cache_table = FAL
 
   } else if (is.null(key)) {
 
-    stop('A Census API key is required.  Obtain one at http://api.census.gov/data/key_signup.html, and then supply the key to the `census_api_key` function to use it throughout your tidycensus session.')
+    stop('A Census API key is required.  Obtain one at http://api.census.gov/data/key_signup.html, and then supply the key to the `census_api_key()` function to use it throughout your tidycensus session.')
 
   }
 
