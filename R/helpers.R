8830cf9b9270bc3750be41f6fbf82992dc330afe
Kyle Walker <kyle@walker-data.com>
2024-09-19 15:46:52 -0500

    support ddhcb, sdhc data for 2020

--- R/helpers.R
+++ R/helpers.R
@@ -662,7 +662,7 @@ summary_files <- function(year) {
                   "cd115", "cd115profile", "cd116")
   } else if (year == 2020) {
     sumfiles <- c("pl", "dhc", "dp", "pes", "dpas", "ddhca",
-                  "dpmp", "dpgu", "dpvi",
+                  "dpmp", "dpgu", "dpvi", "ddhcb", "sdhc",
                   "dhcvi", "dhcgu", "dhcvi", "dhcas", "cd118")
   } else {
     stop("Invalid year for `get_decennial()`; valid years are 2000, 2010, and 2020.", call. = FALSE)
